faca85bb23672207f635abdfd10413c5aef16f6f
AlexisRoj <alexisroj2@gmail.com>
2017-05-19 00:26:34 -0500

    Funcionando Login Google Antes de implementar listar contactos Funcionando fragment contactos Funcionando GeoFence en el Activity

--- app/src/main/java/com/innovagenesis/aplicaciones/android/examendocev3/MainActivity.java
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/examendocev3/MainActivity.java
@@ -1,44 +1,67 @@
 package com.innovagenesis.aplicaciones.android.examendocev3;
 
-import android.*;
 import android.Manifest;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.LocationServices;
 import com.innovagenesis.aplicaciones.android.examendocev3.contactos.ContactosFragment;
+import com.innovagenesis.aplicaciones.android.examendocev3.geofense.GeofenceTransitionsIntentService;
+import com.innovagenesis.aplicaciones.android.examendocev3.geofense.SimpleGeofence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.innovagenesis.aplicaciones.android.examendocev3.geofense.Constants.ANDROID_ID;
+import static com.innovagenesis.aplicaciones.android.examendocev3.geofense.Constants.ANDROID_LATITUDE;
+import static com.innovagenesis.aplicaciones.android.examendocev3.geofense.Constants.ANDROID_LOITERING_DELAY;
+import static com.innovagenesis.aplicaciones.android.examendocev3.geofense.Constants.ANDROID_LONGITUDE;
+import static com.innovagenesis.aplicaciones.android.examendocev3.geofense.Constants.ANDROID_RADIUS_METERS;
+import static com.innovagenesis.aplicaciones.android.examendocev3.geofense.Constants.GEOFENCE_EXPIRATION_TIME;
 
 public class MainActivity extends AppCompatActivity
-        implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+        implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener,
+        GoogleApiClient.ConnectionCallbacks {
 
     public static final String TAG = MainActivity.class.getSimpleName();
     private GoogleApiClient mGoogleApiClient;
+    private GoogleApiClient mApiClient;
     public static final int SIGN_IN_GOOGLE_REQUEST_CODE = 1;
     private TextView textLogin;
 
+    private SimpleGeofence mAndroidGeofence;
+    private List<Geofence> mGeofence;
+    private PendingIntent mGeofenceRequestIntent;
+
     private static final int REQUEST_CODE = 1;
     private static final String[] PERMISOS = {
-            android.Manifest.permission.READ_CONTACTS
+            android.Manifest.permission.READ_CONTACTS,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
     };
 
     private FloatingActionButton fab;
@@ -48,7 +71,9 @@ public class MainActivity extends AppCompatActivity
 
         //Pide los permisos
         int leer = ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
-        if (leer == PackageManager.PERMISSION_DENIED) {
+        int gps = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if ((leer == PackageManager.PERMISSION_DENIED) || (gps == PackageManager.PERMISSION_DENIED)) {
             ActivityCompat.requestPermissions(this, PERMISOS, REQUEST_CODE);
         }
 
@@ -56,7 +81,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
         textLogin = (TextView) findViewById(R.id.txtview_email); //Muestra correo
 
@@ -74,7 +98,32 @@ public class MainActivity extends AppCompatActivity
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
+        if (!disponiblesGooglePlayServices()){
+            Toast.makeText(MainActivity.this,
+                    "Servicios de Google Play no disponibles", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+
+
     }
+
+    public void crearGeofences(){
+        mAndroidGeofence = new SimpleGeofence(
+                ANDROID_ID,
+                ANDROID_LATITUDE,
+                ANDROID_LONGITUDE,
+                ANDROID_RADIUS_METERS,
+                GEOFENCE_EXPIRATION_TIME,
+                Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT | Geofence.GEOFENCE_TRANSITION_DWELL,
+                ANDROID_LOITERING_DELAY
+        );
+
+        mGeofence.add(mAndroidGeofence.toGeofence());
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -94,13 +143,12 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
     /**
      * Listener selector de los botones
-     * */
+     */
     @Override
     public void onClick(View v) {
 
@@ -117,55 +165,66 @@ public class MainActivity extends AppCompatActivity
     /**
      * Método encargado de gestionar los elementos del cerrar
      * sesión de Google
-     * */
+     */
     private void mCerrarSesionGoogle() {
         Auth.GoogleSignInApi.signOut(mGoogleApiClient)
                 .setResultCallback(new ResultCallback<Status>() {
-            @Override
-            public void onResult(@NonNull Status status) {
-                //Limpia el textview
-                textLogin.setText(null);
-                fab.setVisibility(View.GONE);
-            }
-        });
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        //Limpia el textview
+                        textLogin.setText(null);
+                        fab.setVisibility(View.GONE);
+                    }
+                });
     }
 
     /**
      * Método encargado de gestionar los elementos del inicio
      * de sesión de Google
-     * */
+     */
     private void mInicioSeccionGoogle() {
         Intent signIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signIntent, SIGN_IN_GOOGLE_REQUEST_CODE);
     }
 
-
     /**
      * Encargada de esperar los resultados de Google y Facebook
      * Login con la api
-     * */
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         //Valida el login de google
-        if (requestCode == SIGN_IN_GOOGLE_REQUEST_CODE){
+        if (requestCode == SIGN_IN_GOOGLE_REQUEST_CODE) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleSignInResult(result);
         }
-
     }
 
     /***
      * Si el sign in con google es exito ejecuta este método
      * */
     private void handleSignInResult(GoogleSignInResult result) {
-        Log.d(TAG,"handleSignInResult: "+result.isSuccess());
-        if (result.isSuccess()){
+        Log.d(TAG, "handleSignInResult: " + result.isSuccess());
+        if (result.isSuccess()) {
             GoogleSignInAccount account = result.getSignInAccount();
-            if (account!= null){
-                textLogin.setText(account.getEmail());
+            if (account != null) {
 
+                mApiClient = new GoogleApiClient.Builder(this)
+                        .addApi(LocationServices.API)
+                        .addConnectionCallbacks(this)
+                        .addOnConnectionFailedListener(this)
+                        .build();
+
+                mApiClient.connect();
+
+                mGeofence = new ArrayList<>();
+                crearGeofences();
+
+
+
+                textLogin.setText(account.getEmail());
 
                 fab = (FloatingActionButton) findViewById(R.id.fab);
 
@@ -175,29 +234,59 @@ public class MainActivity extends AppCompatActivity
                     public void onClick(View view) {
 
                         ContactosFragment fragment = new ContactosFragment();
-                        FragmentManager  fragmentManager = getSupportFragmentManager();
+                        FragmentManager fragmentManager = getSupportFragmentManager();
 
                         fragmentManager.beginTransaction()
-                                .replace(R.id.contenedor,fragment)
+                                .replace(R.id.contenedor, fragment)
                                 .addToBackStack(null)
                                 .commit();
-
                     }
                 });
-
-
-
-
             }
         }
     }
 
     /**
      * Valida error de conexion a Google
-     * */
+     */
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         Log.v(TAG, "onConnectionFailed: Error conectando cuenta Google, " +
                 connectionResult.getErrorMessage());
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        int leer = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if(leer == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this,PERMISOS,1);
+        }
+
+        mGeofenceRequestIntent = getGeofenceTransitionPendingIntent();
+        LocationServices.GeofencingApi.addGeofences(mApiClient,mGeofence,mGeofenceRequestIntent);
+
+        Toast.makeText(MainActivity.this, "Iniciando servicio de Geofence", Toast.LENGTH_SHORT).show();
+
+        //finish();
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    private boolean disponiblesGooglePlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (ConnectionResult.SUCCESS == resultCode) {
+            Toast.makeText(MainActivity.this, "Servicios de Google Play disponibles", Toast.LENGTH_SHORT).show();
+            return true;
+        } else return false;
+    }
+
+    private PendingIntent getGeofenceTransitionPendingIntent() {
+        Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
+        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 }
